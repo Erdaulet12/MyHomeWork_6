b8199b060e46095c5d37a8fcdd87dc1da717651e
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-02-18 13:03:40 +0600

    task_3

--- task_3.py
+++ task_3.py
@@ -1,17 +1,25 @@
+"""task_3.py"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-url = "https://finance.kapital.kz/"
 
-driver = webdriver.Chrome()
-driver.get(url)
+def main():
+    """main method"""
+
+    url = "https://finance.kapital.kz/"
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    currency_element = driver.find_element(
+        By.CSS_SELECTOR, "table.currencies-table")
+
+    usd_rate = currency_element.text
+    driver.quit()
+    return usd_rate
 
 
-currency_element = driver.find_element(
-    By.CSS_SELECTOR, "table.currencies-table")
-
-usd_rate = currency_element.text
-
-print(usd_rate)
-
-driver.quit()
+if __name__ == "__main__":
+    usd_rate = main()
+    print(usd_rate)
